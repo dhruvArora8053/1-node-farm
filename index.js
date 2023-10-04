7891e8f36dc3bc86a3ca81e6da8bae8ffc692615
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-04 19:44:39 +0530

    implemented overview

--- index.js
+++ index.js
@@ -2,43 +2,63 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// // console.log(textIn);
+const replaceTemplate = (temp, product) => {
+  let output = temp.replaceAll("{%PRODUCTNAME%}", product.productName);
+  output = output.replaceAll("{%IMAGE%}", product.image);
+  output = output.replaceAll("{%PRICE%}", product.price);
+  output = output.replaceAll("{%FROM%}", product.image);
+  output = output.replaceAll("{%NUTRIENTS%}", product.image);
+  output = output.replaceAll("{%QUANTITY%}", product.image);
+  output = output.replaceAll("{%DESCRIPTION%}", product.image);
+  output = output.replaceAll("{%ID%}", product.image);
 
-// const textOut = `This is what we know about the avocado: ${textIn}\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File written!");
+  if (!product.organic)
+    output = output.replaceAll("{%NOT_ORGANIC%}", "not-organic");
 
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
+  return output;
+};
 
-//       fs.writeFile(
-//         ".txt/final.txt",
-//         `${data2}\n${data3}`,
-//         "utf-8",
-//         (err) => {}
-//       );
-//     });
-//   });
-// });
-// console.log("Will read file");
-
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+console.log(tempCard);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataO = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    // console.log(cardsHtml);
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    console.log(output);
+    res.end(output);
+
+    // Product Page
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+
+    // API
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+
+    // NOT Found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
