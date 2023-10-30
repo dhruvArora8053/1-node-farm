95d55a36f0baf2e0f182deebf34de1941beed0bf
Dhruv Arora <dhruvarora.code@gmail.com>
2023-10-30 17:08:38 +0530

    sfed

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8" 
+  "utf-8"
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
@@ -18,6 +19,11 @@ const tempProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName), { lower: true });
+console.log(slugs);
+
+console.log(slugify("Frish Avocados", { lower: true }));
 
 const server = http.createServer((req, res) => {
   // console.log(req.url);
